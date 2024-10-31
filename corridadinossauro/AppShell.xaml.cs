a281b29b45d735e5602d6544aed9f6a52dd073e3
duda <duduzinhacb@gmail.com>
2024-10-31 09:04:37 -0300

    arrumar arvore

---
+++ corridadinossauro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace corridadinossauro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ corridadinossauro/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace corridadinossauro;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
